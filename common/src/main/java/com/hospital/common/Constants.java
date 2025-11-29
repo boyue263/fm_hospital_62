9ac2c228568f15c493481a646dbdd81ad766bade
boyue <2711822921@qq.com>
2025-11-29 18:48:20 +0800

    feat: add nurse role constant in feature branch

--- common/src/main/java/com/hospital/common/Constants.java
+++ common/src/main/java/com/hospital/common/Constants.java
@@ -4,5 +4,5 @@ package com.hospital.common;
 public class Constants {
     public static final String ROLE_ADMIN = "ADMIN";
     public static final String ROLE_DOCTOR = "DOCTOR";
-    // 这里故意留空，后续制造冲突
+    public static final String ROLE_NURSE = "NURSE";  // feature分支添加
 }

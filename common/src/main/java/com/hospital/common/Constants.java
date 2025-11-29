a45b7016006ceb2cdfa3c5063914eb50a1360342
boyue <2711822921@qq.com>
2025-11-29 18:47:00 +0800

    refactor: add patient role constant in develop branch

---
+++ common/src/main/java/com/hospital/common/Constants.java
@@ -0,0 +1,7 @@
+package com.hospital.common;
+
+public class Constants {
+    public static final String ROLE_ADMIN = "ADMIN";
+    public static final String ROLE_DOCTOR = "DOCTOR";
+    public static final String ROLE_PATIENT = "PATIENT";  // develop分支添加
+}
